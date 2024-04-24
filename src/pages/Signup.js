40203318cef91af1965b01eaab9497a3aa8a7cea
amitark7 <kumaramit21470@gmail.com>
2024-04-24 18:40:22 +0530

    change modal now it is open in center

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -56,17 +56,17 @@ const Signup = () => {
   return (
     <div className="flex flex-col items-center justify-center">
       {showModal && (
-        <div className="fixed inset-0 flex h-[200px] mt-4 justify-center items-center z-10 text-black">
+        <div className="fixed inset-0 flex justify-center items-center z-10">
           <div className="modal-overlay fixed inset-0 bg-gray-500 opacity-50"></div>
-          <div className="modal-container bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto">
+          <div className="modal-container bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto flex flex-col items-center">
             <div className="modal-content py-6 text-left px-4">
               <div className="flex justify-between items-center pb-3">
                 <p className="text-2xl font-bold mb-3">Successfully !</p>
               </div>
               <p className="mb-3">
-                User signup Succesfully ! Click Ok to Redirect Login Page.
+                User signup Succesfully! Click Ok to Redirect Login Page.
               </p>
-              <div className="mt-7 flex justify-end ">
+              <div className="mt-7 flex justify-end">
                 <button
                   onClick={handleNavigate}
                   className="px-4 py-2 text-gray-800 rounded-md bg-green-300 hover:bg-green-400"
